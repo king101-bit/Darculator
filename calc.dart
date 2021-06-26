20de92daecafb2570e5fcd11ea44ef752d1143ee
Zack agba <zackagba7@gmail.com>
2021-06-26 05:54:52 +0100

    Emoji's added🌠

--- calc.dart
+++ calc.dart
@@ -13,7 +13,6 @@ class Calc {
     this.num2 = secondNum;
   }
 
-  //class method
   //class methods
   double add(double firstNum, double secondNum) {
     return firstNum + secondNum;
@@ -40,7 +39,7 @@ class Calc {
   }
 
   static void backToMainMenu() {
-    print("\nReturning to Base...\n");
+    print("\nReturning to HQ....\n");
     wholeThing();
   }
 
@@ -48,15 +47,16 @@ class Calc {
     // TODO : adding a Delay
 
     print("""
-        \nWelcome to the Darculator!
-        \n=============================== 
-        \nThis calculator performs math operation. 
-        \nPlease type your first digit \nType 'exit' to exit at anytime! \n""");
+        \nWelcome to the Darculator ⚡!
+        \n===============================
+        \nThis calculator performs mathematcal operations 👨🏾‍💻.
+        \nThis calculator was made in association with jeanluc 🌠. 
+        \nPlease type your first digit \nType 'exit' to Stop the program at anytime!❌ \n""");
 
     String firstInput =
         stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
     firstInput = firstInput.trim();
-    double firstNum = 0.0;
+    double firstNum = 0;
 
     if (firstInput == "exit") {
       exit(0);
@@ -64,12 +64,12 @@ class Calc {
       firstNum = double.parse(firstInput);
     }
 
-    print("\nGreat! Now input your second number.\n");
+    print("\nGreat! Now input your second digit.\n");
 
     String secondInput =
         stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
     secondInput = secondInput.trim();
-    double secondNum = 0.0;
+    double secondNum = 0;
 
     if (secondInput == "exit") {
       exit(0);
@@ -79,42 +79,47 @@ class Calc {
 
     Calc calc1 = new Calc(firstNum, secondNum);
 
-    print("""\nGreat! Now, choose the digit you want.
-    \nType + for addition
-    \nType - for subtraction
-    \nType * for multiplication
-    \nType ** to raise the first number to the power of the second
-    \nType / to divide""");
+    print("""\nAwesome! Now, choose the formulae you want🔮.
+    \nType + for addition ➕
+    \nType - for subtraction ➖
+    \nType * for multiplication ✖️
+    \nType ** to raise the first number to the power of the second 🤷🏾‍♂️
+    \nType / to divide ➗""");
 
     String operator = stdin.readLineSync(encoding: Encoding.getByName("utf-8"));
     operator = operator.trim();
 
     switch (operator) {
       case "+":
-        print("${calc1.num1} plus ${calc1.num2} equals = ${calc1.add(firstNum, secondNum)}");
+        print(
+            "${calc1.num1} plus ${calc1.num2} equals = ${calc1.add(firstNum, secondNum)}");
         backToMainMenu();
         break;
       case "-":
-        print("${calc1.num1} minus ${calc1.num2} equals = ${calc1.subtract(firstNum, secondNum)}");
+        print(
+            "${calc1.num1} minus ${calc1.num2} equals = ${calc1.subtract(firstNum, secondNum)}");
         backToMainMenu();
         break;
       case "*":
-        print("${calc1.num1} multiplied by ${calc1.num2} equals = ${calc1.multiply(firstNum, secondNum)}");
+        print(
+            "${calc1.num1} multiplied by ${calc1.num2} equals = ${calc1.multiply(firstNum, secondNum)}");
         backToMainMenu();
         break;
       case "**":
-        print("${calc1.num1} raised to the power of ${calc1.num2} equals = ${calc1.expontentiate(firstNum, secondNum)}");
+        print(
+            "${calc1.num1} raised to the power of ${calc1.num2} equals = ${calc1.expontentiate(firstNum, secondNum)}");
         backToMainMenu();
         break;
       case "/":
-        print("${calc1.num1} divided by ${calc1.num2} equals = ${calc1.divide(firstNum, secondNum)}");
+        print(
+            "${calc1.num1} divided by ${calc1.num2} equals = ${calc1.divide(firstNum, secondNum)}");
         backToMainMenu();
         break;
       case "exit":
         exit(0);
         break;
       default:
-        print("Syntax error");
+        print("ERROR:Bad expression...🌋");
         backToMainMenu();
         break;
     }
